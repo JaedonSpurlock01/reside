a2bc55166cffcbf7508dfeeb258bb89600903cff
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2024-04-15 17:06:26 -0700

    feat: added empty state for no results after filter

--- components/listings/ListContainer.tsx
+++ components/listings/ListContainer.tsx
@@ -4,6 +4,7 @@ import FilterHeader from "./filter/FilterHeader";
 import Grid from "./Grid";
 import { ListingPagination } from "./ListingPagination";
 import { RentCastListing } from "@/types/RentCastListing";
+import EmptyState from "../EmptyState";
 
 interface ListContainerProps {
   selectedCity?: string;
@@ -32,11 +33,13 @@ export default function ListContainer({
         <div className="flex flex-col items-center justify-center mt-10">
           <CircularProgress size={20} thickness={5} />
         </div>
-      ) : (
+      ) : listings.length > 0 ? (
         <>
           <Grid listings={listings} />
           <ListingPagination />
         </>
+      ) : (
+        <EmptyState />
       )}
     </div>
   );
